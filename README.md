2472c5e127d8fb1720d9cc78981372d7e4cb0133
Martin Hradil <mhradil@redhat.com>
2024-06-20 17:52:10 +0000

    add README w/ run instructions

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # pulp-ui
 a community driven UI for Pulp
+
+## How to run
+
+### backend
+
+You can follow the [pulp-oci-images quickstart](https://pulpproject.org/pulp-oci-images/docs/admin/tutorials/quickstart/),
+TLDR:
+
+setup:
+
+```
+mkdir -p ~/pulp-backend-oci/{settings/certs,pulp_storage,pgsql,containers}
+cd ~/pulp-backend-oci/
+echo "
+CONTENT_ORIGIN='http://$(hostname):8080'
+ANSIBLE_API_HOSTNAME='http://$(hostname):8080'
+ANSIBLE_CONTENT_HOSTNAME='http://$(hostname):8080/pulp/content'
+" >> settings/settings.py
+```
+
+run:
+
+```
+cd ~/pulp-backend-oci/
+podman run --publish 8080:80 \
+           --volume "$(pwd)/settings":/etc/pulp \
+           --volume "$(pwd)/pulp_storage":/var/lib/pulp \
+           --volume "$(pwd)/pgsql":/var/lib/pgsql \
+           --volume "$(pwd)/containers":/var/lib/containers \
+           --device /dev/fuse \
+           docker.io/pulp/pulp
+```
+
+### frontend
+
+```
+npm install
+npm run start
+```
+
+and open http://localhost:8002/ :tada: :)
--- config/start.config.js
+++ config/start.config.js
@@ -2,39 +2,35 @@ const webpackBase = require('./shared.config');
 
 // Used for getting the correct host when running in a container
 const proxyHost = process.env.API_PROXY_HOST || 'localhost';
-const proxyPort = process.env.API_PROXY_PORT || '55001';
-const apiBasePath = process.env.API_BASE_PATH || '/api/galaxy/';
+const proxyPort = process.env.API_PROXY_PORT || '8080';
+const apiBasePath = process.env.API_BASE_PATH || '/pulp/api/v3';
+const proxyTarget = `http://${proxyHost}:${proxyPort}`;
 
 module.exports = webpackBase({
-  // The host where the API lives. EX: https://localhost:55001
+  // The host where the API lives. EX: http://localhost:8080
   API_HOST: '',
 
-  // Path to the API on the API host. EX: /api/automation-hub
+  // Path to the API on the API host. EX: /pulp/api/v3
   API_BASE_PATH: apiBasePath,
 
-  // Path on the host where the UI is found. EX: /apps/automation-hub
+  // Path on the host where the UI is found. EX: /ui/
   UI_BASE_PATH: '/ui/',
 
-  // Port that the UI is served over
+  // Port that the UI is served over.
   UI_PORT: 8002,
 
   // Serve the UI over http or https. Options: true, false
   UI_USE_HTTPS: false,
 
-  // Enables webpack debug mode. Options: true, false
-  UI_DEBUG: true,
-
-  // Login URI to allow stand alone with and without keycloak
-  UI_EXTERNAL_LOGIN_URI: '/login',
-
   // Value for webpack.devServer.proxy
   // https://webpack.js.org/configuration/dev-server/#devserverproxy
   // used to get around CORS requirements when running in dev mode
   WEBPACK_PROXY: {
-    '/api/': `http://${proxyHost}:${proxyPort}`,
-    '/pulp/api/': `http://${proxyHost}:${proxyPort}`,
-    '/v2/': `http://${proxyHost}:${proxyPort}`,
-    '/extensions/v2/': `http://${proxyHost}:${proxyPort}`,
-    '/static/rest_framework/': `http://${proxyHost}:${proxyPort}`,
+    '/api/': proxyTarget,
+    '/assets/': proxyTarget,
+    '/extensions/': proxyTarget,
+    '/pulp/': proxyTarget,
+    '/static/rest_framework/': proxyTarget,
+    '/v2/': proxyTarget,
   },
 });
