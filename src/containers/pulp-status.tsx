c17fa11a15921a586abf977ffe21d11baae33095
Martin Hradil <mhradil@redhat.com>
2024-07-06 01:10:50 +0000

    PulpStatus: more info

--- src/containers/pulp-status.tsx
+++ src/containers/pulp-status.tsx
@@ -1,11 +1,22 @@
 import { t } from '@lingui/macro';
-import { Card, CardBody, CardTitle, Progress } from '@patternfly/react-core';
-import React, { Component } from 'react';
+import {
+  Card,
+  CardBody,
+  CardExpandableContent,
+  CardHeader,
+  CardTitle,
+  Gallery,
+  GalleryItem,
+  Progress,
+} from '@patternfly/react-core';
+import { sortBy } from 'lodash';
+import React, { Component, type ReactNode, useState } from 'react';
 import { PulpStatusAPI } from 'src/api';
 import {
   AlertList,
   type AlertType,
   BaseHeader,
+  DateComponent,
   LoadingSpinner,
   Main,
   closeAlert,
@@ -21,6 +32,186 @@ interface IState {
   alerts: AlertType[];
   status?;
 }
+
+const AsJSON = ({ data }: { data }) => (
+  <pre style={{ whiteSpace: 'pre-wrap' }}>{JSON.stringify(data, null, 2)}</pre>
+);
+
+const StatusVersions = ({
+  versions,
+}: {
+  versions: { component; version }[];
+}) => {
+  const sorted = sortBy(versions, 'component');
+
+  return (
+    <table style={{ width: '100%' }}>
+      {sorted.map(({ component, version }) => (
+        <tr key={component}>
+          <td>{component}</td>
+          <td>{version}</td>
+        </tr>
+      ))}
+    </table>
+  );
+};
+
+const StatusOnlineWorkers = ({
+  onlineWorkers,
+}: {
+  onlineWorkers: {
+    current_task;
+    last_heartbeat;
+    name;
+    pulp_created;
+    pulp_last_updated;
+  }[];
+}) => (
+  <>
+    {onlineWorkers.map(
+      (
+        { current_task, last_heartbeat, name, pulp_created, pulp_last_updated },
+        i,
+      ) => (
+        <div key={name} style={{ paddingTop: i ? '8px' : '0' }}>
+          <strong>{t`Name`}</strong>: {name}
+          <br />
+          <strong>{t`Created`}</strong>: <DateComponent date={pulp_created} />
+          <br />
+          <strong>{t`Last updated`}</strong>:{' '}
+          <DateComponent date={pulp_last_updated} />
+          <br />
+          <strong>{t`Last heartbeat`}</strong>:{' '}
+          <DateComponent date={last_heartbeat} />
+          <br />
+          {/* TODO: link to tasks, show name */}
+          <strong>{t`Current task`}</strong>:{' '}
+          {current_task ? <AsJSON data={current_task} /> : t`None`}
+        </div>
+      ),
+    )}
+  </>
+);
+
+const StatusOnlineApps = ({ apps }: { apps: { name; last_heartbeat }[] }) => (
+  <>
+    {apps.map(({ name, last_heartbeat }, i) => (
+      <div key={name} style={{ paddingTop: i ? '8px' : '0' }}>
+        <strong>{t`Name`}</strong>: {name}
+        <br />
+        <strong>{t`Last heartbeat`}</strong>:{' '}
+        <DateComponent date={last_heartbeat} />
+      </div>
+    ))}
+  </>
+);
+
+const StatusOnlineApiApps = ({
+  onlineApiApps,
+}: {
+  onlineApiApps: { name; last_heartbeat }[];
+}) => <StatusOnlineApps apps={onlineApiApps} />;
+
+const StatusOnlineContentApps = ({
+  onlineContentApps,
+}: {
+  onlineContentApps: { name; last_heartbeat }[];
+}) => <StatusOnlineApps apps={onlineContentApps} />;
+
+const StatusDatabaseConnection = ({
+  databaseConnection,
+}: {
+  databaseConnection: { connected: boolean };
+}) => (databaseConnection?.connected ? t`Connected` : `Not connected`);
+
+const StatusRedisConnection = ({
+  redisConnection,
+}: {
+  redisConnection: { connected: boolean };
+}) => (redisConnection?.connected ? t`Connected` : `Not connected`);
+
+const StatusStorage = ({ storage }: { storage: { total; used; free } }) => {
+  const value = (100 / storage.total) * storage.used;
+  const total = getHumanSize(storage.total);
+  const used = getHumanSize(storage.used);
+  const free = getHumanSize(storage.free);
+
+  return (
+    <>
+      <Progress
+        value={value}
+        title={t`Storage`}
+        variant={
+          value > 88
+            ? 'danger'
+            : value > 66
+              ? 'warning'
+              : value > 33
+                ? null
+                : 'success'
+        }
+      />
+      <br />
+      <strong>{t`Total`}</strong>: {total}
+      <br />
+      <strong>{t`Used`}</strong>: {used}
+      <br />
+      <strong>{t`Free`}</strong>: {free}
+    </>
+  );
+};
+
+const StatusContentSettings = ({
+  contentSettings,
+}: {
+  contentSettings: { content_origin; content_path_prefix };
+}) => (
+  <>
+    <p>
+      <strong>{t`Origin`}</strong>:{' '}
+      <code>{contentSettings.content_origin}</code>
+    </p>
+    <p>
+      <strong>{t`Path prefix`}</strong>:{' '}
+      <code>{contentSettings.content_path_prefix}</code>
+    </p>
+  </>
+);
+
+const StatusDomainEnabled = ({ domainEnabled }: { domainEnabled: boolean }) =>
+  domainEnabled ? t`Enabled` : t`Not enabled`;
+
+const CardItem = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: ReactNode;
+}) => (
+  <GalleryItem>
+    <Card>
+      <CardTitle>{title}</CardTitle>
+      <CardBody>{children}</CardBody>
+    </Card>
+  </GalleryItem>
+);
+
+const CardJSON = ({ status }: { status }) => {
+  const [expanded, setExpanded] = useState<boolean>(false);
+
+  return (
+    <Card isExpanded={expanded}>
+      <CardHeader onExpand={() => setExpanded(!expanded)}>
+        <CardTitle onClick={() => setExpanded(!expanded)}>{t`JSON`}</CardTitle>
+      </CardHeader>
+      <CardExpandableContent>
+        <CardBody>
+          <AsJSON data={status} />
+        </CardBody>
+      </CardExpandableContent>
+    </Card>
+  );
+};
 
 class PulpStatus extends Component<RouteProps, IState> {
   constructor(props) {
@@ -39,11 +230,6 @@ class PulpStatus extends Component<RouteProps, IState> {
   render() {
     const { alerts, status } = this.state;
 
-    const value = status
-      ? (100 / status.storage.total) * status.storage.used
-      : 0;
-    const free = status ? getHumanSize(status.storage.free) : 0;
-
     return (
       <>
         <AlertList
@@ -56,42 +242,53 @@ class PulpStatus extends Component<RouteProps, IState> {
           }
         />
         <BaseHeader title={t`Status`} />
-        <Main>
-          <Card>
-            <section className='pulp-section pf-v5-c-content'>
-              <CardTitle>
-                <h2>{t`TODO`}</h2>
-              </CardTitle>
-              <CardBody>
-                {!status ? (
-                  <LoadingSpinner />
-                ) : (
-                  <>
-                    <Progress
-                      value={value}
-                      title={t`Storage`}
-                      variant={
-                        value > 88
-                          ? 'danger'
-                          : value > 66
-                            ? 'warning'
-                            : value > 33
-                              ? null
-                              : 'success'
-                      }
-                    />
-                    <br />
-                    {t`Free`} {free}
-                    <br />
-                    TODO versions, workers.. .. and enable/disable menu based on
-                    .versions
-                    <pre>{JSON.stringify(status, null, 2)}</pre>
-                  </>
-                )}
-              </CardBody>
-            </section>
-          </Card>
-        </Main>
+        {!status ? (
+          <Main>
+            <LoadingSpinner />
+          </Main>
+        ) : (
+          <Main>
+            <Gallery hasGutter>
+              <CardItem title={t`Versions`}>
+                <StatusVersions versions={status?.versions} />
+              </CardItem>
+              <CardItem title={t`Storage`}>
+                <StatusStorage storage={status?.storage} />
+              </CardItem>
+              <CardItem title={t`Content settings`}>
+                <StatusContentSettings
+                  contentSettings={status?.content_settings}
+                />
+              </CardItem>
+              <CardItem title={t`Database connection`}>
+                <StatusDatabaseConnection
+                  databaseConnection={status?.database_connection}
+                />
+              </CardItem>
+              <CardItem title={t`Redis connection`}>
+                <StatusRedisConnection
+                  redisConnection={status?.redis_connection}
+                />
+              </CardItem>
+              <CardItem title={t`Domain enabled`}>
+                <StatusDomainEnabled domainEnabled={status?.domain_enabled} />
+              </CardItem>
+              <CardItem title={t`Online workers`}>
+                <StatusOnlineWorkers onlineWorkers={status?.online_workers} />
+              </CardItem>
+              <CardItem title={t`Online api apps`}>
+                <StatusOnlineApiApps onlineApiApps={status?.online_api_apps} />
+              </CardItem>
+              <CardItem title={t`Online content apps`}>
+                <StatusOnlineContentApps
+                  onlineContentApps={status?.online_content_apps}
+                />
+              </CardItem>
+            </Gallery>
+            <br />
+            <CardJSON status={status} />{' '}
+          </Main>
+        )}
       </>
     );
   }
