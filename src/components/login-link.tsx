0306447a56dddbda62d62900c80edb3ba6e9f95d
Martin Hradil <mhradil@redhat.com>
2024-10-18 20:35:23 +0000

    Make API_BASE_PATH, UI_BASE_PATH, UI_EXTERNAL_LOGIN_URI & EXTRA_VERSION configurable via /pulp-ui-config.json

--- src/components/login-link.tsx
+++ src/components/login-link.tsx
@@ -2,6 +2,8 @@ import { t } from '@lingui/macro';
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Paths, formatPath } from 'src/paths';
+import { config } from 'src/ui-config';
+import { loginURL } from 'src/utilities';
 
 interface IProps {
   button?: boolean;
@@ -9,15 +11,16 @@ interface IProps {
 
 export const LoginLink = ({ button }: IProps) => {
   const { pathname } = useLocation();
+  const className = button ? 'pf-v5-c-button pf-m-primary' : '';
 
   // NOTE: also update AuthHandler#render (src/routes.tsx) when changing this
-  //if (featureFlags?.external_authentication && UI_EXTERNAL_LOGIN_URI) {
-  //  return <a className={className} href={loginURL(pathname)}>{t`Login`}</a>;
-  //}
+  if (config.UI_EXTERNAL_LOGIN_URI) {
+    return <a className={className} href={loginURL(pathname)}>{t`Login`}</a>;
+  }
 
   return (
     <Link
-      className={button ? 'pf-v5-c-button pf-m-primary' : ''}
+      className={className}
       to={formatPath(Paths.meta.login, {}, { next: pathname })}
     >{t`Login`}</Link>
   );
--- src/index.d.ts
+++ src/index.d.ts
@@ -2,13 +2,10 @@
 // This will tell typescript that anything from this module is of type any.
 
 declare module '*.png';
+declare module '*.svg';
 
 // Declare configuration globals here so that TypeScript compiles
 /* eslint-disable no-var */
-declare var API_BASE_PATH;
-declare var API_HOST;
 declare var APPLICATION_NAME;
-declare var UI_BASE_PATH;
 declare var UI_BUILD_INFO;
 declare var UI_DOCS_URL;
-declare var UI_EXTERNAL_LOGIN_URI;
