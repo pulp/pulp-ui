e2208fbe6a7df4cfbd0df3875ae88517db1b3e5a
Martin Hradil <mhradil@redhat.com>
2024-10-20 20:56:15 +0000

    move caching logic to utils

--- src/api/pulp-status.ts
+++ src/api/pulp-status.ts
@@ -1,24 +1,8 @@
 import { PulpAPI } from './pulp';
 
 class API extends PulpAPI {
-  last_status = null;
-  last_status_time = 0;
-
-  private _get() {
-    return this.http.get('status/');
-  }
-
   get() {
-    this.last_status_time = Date.now();
-    this.last_status = this._get();
-    return this.last_status;
-  }
-
-  cache() {
-    if (!this.last_status || this.last_status_time + 300000 < Date.now()) {
-      return this.get();
-    }
-    return this.last_status;
+    return this.http.get('status/');
   }
 }
 
