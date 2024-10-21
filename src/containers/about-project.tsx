2efaa2572a2d386b3799d12b2ae145730cce31bb
ZitaNemeckova <znemecko@redhat.com>
2024-10-22 00:59:46 +0200

    Add About page (#95)
    * Add About page
    
    * Add docker version  of changing password to README
    
    * Update src/containers/about-project.tsx
    
    Co-authored-by: Gerrod <gerrodubben@gmail.com>
    
    * Update src/containers/about-project.tsx
    
    Co-authored-by: Gerrod <gerrodubben@gmail.com>
    
    * Update src/containers/about-project.tsx
    
    Co-authored-by: Gerrod <gerrodubben@gmail.com>
    
    * Update src/containers/about-project.tsx
    
    Co-authored-by: Gerrod <gerrodubben@gmail.com>
    
    * prettier
    
    * l10n, linters
    
    ---------
    
    Co-authored-by: Gerrod <gerrodubben@gmail.com>
    Co-authored-by: Martin Hradil <mhradil@redhat.com>

--- src/containers/about-project.tsx
+++ src/containers/about-project.tsx
@@ -1,25 +1,78 @@
 import { t } from '@lingui/macro';
-import { Gallery } from '@patternfly/react-core';
-import React, { Component } from 'react';
-import { BaseHeader, Main } from 'src/components';
+import {
+  Brand,
+  Card,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Gallery,
+} from '@patternfly/react-core';
+import React from 'react';
+import { BaseHeader, ExternalLink, Main } from 'src/components';
 import { type RouteProps, withRouter } from 'src/utilities';
+import PulpLogo from 'static/images/pulp_logo.png';
 
-class AboutProject extends Component<RouteProps> {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <>
-        <BaseHeader title={t`About project`} />
-        <Main>
-          <Gallery hasGutter></Gallery>
-          <br />
-        </Main>
-      </>
-    );
-  }
-}
+const AboutProject = (_props: RouteProps) => (
+  <>
+    <BaseHeader title={t`About project`} />
+    <Main>
+      <Card>
+        <CardTitle>
+          <Brand
+            style={{
+              display: 'block',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+            }}
+            src={PulpLogo}
+            alt={t`Pulp logo`}
+          />
+        </CardTitle>
+        <CardBody style={{ textAlign: 'center' }}>
+          {t`Pulp is an open source project that makes it easy for developers to fetch, upload, and distribute Software Packages on-prem or in the cloud.`}
+        </CardBody>
+      </Card>
+      <Card>
+        <CardBody>
+          <Gallery
+            hasGutter
+            style={{ gridTemplateColumns: '32.5% 32.5% 32.5%' }}
+          >
+            <Card>
+              <CardTitle>{APPLICATION_NAME}</CardTitle>
+              <CardBody>{t`This project is an attempt to up-cycle the Ansible Galaxy UI codebase. The first version contains mostly saved code with minor fixes. In the future more types of content will be added. We welcome kindly any contributions.`}</CardBody>
+              <CardFooter>
+                {' '}
+                <ExternalLink href='https://github.com/pulp/pulp-ui'>
+                  {t`GitHub repository`}
+                </ExternalLink>
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardTitle>{t`Issue tracker`}</CardTitle>
+              <CardBody>{t`If you find a bug or have an idea for enhancement, please feel free to file a Github issue. Thank you for contributing to improvement of the project.`}</CardBody>
+              <CardFooter>
+                {' '}
+                <ExternalLink href='https://github.com/pulp/pulp-ui/issues'>
+                  {t`GitHub issues`}
+                </ExternalLink>
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardTitle>{t`Get involved`}</CardTitle>
+              <CardBody>{t`Join our communication channels and get to know the contributors and users of Pulp's strong ecosystem.`}</CardBody>
+              <CardFooter>
+                {' '}
+                <ExternalLink href='https://pulpproject.org/help/community/get-involved/'>
+                  {t`Community`}
+                </ExternalLink>
+              </CardFooter>
+            </Card>
+          </Gallery>
+        </CardBody>
+      </Card>
+    </Main>
+  </>
+);
 
 export default withRouter(AboutProject);
