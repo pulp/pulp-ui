18a7c407d08c0dedc802964c0df9c0f3af480f46
Martin Hradil <mhradil@redhat.com>
2024-11-05 00:06:16 +0000

    workflows/cypress: enable tests

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,4 +1,27 @@
-// https://on.cypress.io/configuration
-import 'cypress-file-upload';
-import './commands';
-import './login';
+// https://on.cypress.io/custom-commands
+
+// const api = Cypress.env('API_BASE_PATH');
+const ui = Cypress.env('UI_BASE_PATH');
+
+Cypress.Commands.add('assertTitle', {}, (title) => {
+  cy.contains('.pf-v5-c-title', title);
+});
+
+Cypress.Commands.add('ui', {}, (path = '') => {
+  cy.visit(ui + path);
+});
+
+Cypress.Commands.add('login', {}, (username, password) => {
+  if (!username && !password) {
+    // default to admin
+    username = Cypress.env('username');
+    password = Cypress.env('password');
+  }
+
+  cy.session(username, () => {
+    window.sessionStorage.credentials = JSON.stringify({
+      username,
+      password,
+    });
+  });
+});
