6f0cf51743dd98c36c5ac242aa05450193cc20f2
Matthias Dellweg <mdellweg@redhat.com>
2024-12-17 19:06:21 +0100

    Reorganize configPromise

--- src/ui-config.ts
+++ src/ui-config.ts
@@ -1,13 +1,13 @@
 import defaults from '../pulp-ui-config.json';
 
-export const configPromise = fetch('/pulp-ui-config.json').then((data) =>
-  data.status > 0 && data.status < 300
-    ? data.json()
-    : Promise.reject(`${data.status}: ${data.statusText}`),
-);
+export const configPromise = fetch('/pulp-ui-config.json')
+  .then((data) =>
+    data.status > 0 && data.status < 300
+      ? data.json()
+      : Promise.reject(`${data.status}: ${data.statusText}`),
+  )
+  .then((data) => (config = data));
 
 export let config = null;
 
 export const configFallback = () => (config = defaults);
-
-configPromise.then((data) => (config = data));
