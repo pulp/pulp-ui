bbb7ec91f69e4e08e2fa8bb9be95131a8705c79a
Martin Hradil <mhradil@redhat.com>
2025-07-23 01:45:14 +0200

    pulp-login: change roles/ to groups/ (#244)
    we still need an api endpoint to check whether we're logged in or not,
    but `/pulp/api/v3/roles/` requires the user to be admin.
    Changing to `/pulp/api/v3/groups/`, as per #198.
    
    (And don't force the db to do work, settle for 0 items.)
    
    Closes #198

--- src/api/pulp-login.ts
+++ src/api/pulp-login.ts
@@ -4,6 +4,9 @@ const base = new PulpAPI();
 
 export const PulpLoginAPI = {
   try: (username, password) =>
-    // roles = any api that will always be there and requires auth
-    base.http.get(`roles/`, { auth: { username, password } }),
+    // groups = any api that will always be there and requires auth
+    base.http.get(`groups/`, {
+      auth: { username, password },
+      params: { limit: 0, offset: 0 },
+    }),
 };
