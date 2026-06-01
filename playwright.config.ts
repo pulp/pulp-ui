d7e462855882eed8d06c94ff1b29ffa0dbd626ed
Wes <76014409+Redtigercod4@users.noreply.github.com>
2026-06-01 15:14:53 +0000

    Added playwright and removed cypress. Added associated configuration.

---
+++ playwright.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './playwright',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: process.env.CI ? 'github' : "html",
+  use: {
+    baseURL: process.env.BASE_URL ?? 'http://localhost:8002',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure'
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    // TODO: Renable when playwright tests are completed
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+  ],
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:8002',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120_000,
+    stdout: "ignore",
+    stderr: "pipe",
+    env: {
+      API_PROXY: process.env.API_PROXY ?? "http://localhost:8080"
+    }
+  },
+});
