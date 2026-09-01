63dcded670fe5236a77fca35a1d01ad50b048080
Wes <76014409+Redtigercod4@users.noreply.github.com>
2026-09-01 17:23:37 +0100

    Add shared base types for Repositories, Publications, Remotes and Distributions (#307)
    * Added new common.ts within API folder to store generic types against pulpcore serializers
    
    * Updated types within common.ts and created a new RPM Repository type based off serializer
    
    * Updated ansible repository to new interface, mapped out shared types outside pulp core within common.ts
    
    * Updated file repository to new interface adding in new fields from recently updated serializer
    
    * Fixed issue with Ansible types. Removed incorrect comment due to plugin confusion for ansible.
    
    * Defined new distribution generic from pulpcore and built out ansible distribution
    
    * Defined new distribution and publication generic for file distribution and publication types
    
    * Defined new Remote generic and updated ansible remote type
    
    * Updated File Remote definition along with commit pinning references.
    
    * Updated Container distributions to have new interface against plugin

--- src/api/rpm-repository.ts
+++ src/api/rpm-repository.ts
@@ -1,7 +1,40 @@
+import type { GenericRepository } from './common';
 import { PulpAPI } from './pulp';
+
+type RPMChecksumType =
+  | 'unknown'
+  | 'md5'
+  | 'sha'
+  | 'sha1'
+  | 'sha224'
+  | 'sha256'
+  | 'sha384'
+  | 'sha512';
+type RPMCompressionType = 'zstd' | 'gz' | 'none';
+type RPMLayoutType = 'nested_alphabetically' | 'flat' | 'nested_by_digest';
+
+/**
+ * RPM Repository Type.
+ *
+ * @see https://github.com/pulp/pulp_rpm/blob/main/pulp_rpm/app/serializers/repository.py
+ */
+interface RPMRepositoryType extends GenericRepository {
+  autopublish?: boolean;
+  metadata_signing_service?: string | null;
+  package_signing_service?: string | null;
+  package_signing_fingerprint?: string | null;
+  retain_package_versions?: number;
+  checksum_type?: RPMChecksumType | null;
+  compression_type?: RPMCompressionType | null;
+  layout?: RPMLayoutType | null;
+  repo_config?: Record<string, unknown>;
+  osv_config?: { name: string; releases: unknown }[] | null;
+}
 
 const base = new PulpAPI();
 
 export const RPMRepositoryAPI = {
   list: (params?) => base.list(`repositories/rpm/rpm/`, params),
 };
+
+export type { RPMRepositoryType };
