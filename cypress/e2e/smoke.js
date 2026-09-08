cb5aad813b5645695cbc16aab2690065c19f2450
Wes <76014409+Redtigercod4@users.noreply.github.com>
2026-09-08 13:32:56 +0100

    chore(major): migrate from cypress v15 to cypress v16 (#358)
    * Updated to Cypress 16, migrated support file to typescript and removed deprecated features
    
    * Migrated Cypress.env in tests to new configuration

--- cypress/e2e/smoke.js
+++ cypress/e2e/smoke.js
@@ -13,8 +13,10 @@ describe('UI smoke tests', () => {
     cy.assertTitle('Login');
 
     // fill in form manually, submit
-    cy.get('#pf-login-username-id').type(Cypress.env('username'));
-    cy.get('#pf-login-password-id').type(Cypress.env('password'));
+    cy.env(['username', 'password']).then(({ username, password }) => {
+      cy.get('#pf-login-username-id').type(username);
+      cy.get('#pf-login-password-id').type(password);
+    });
     cy.contains('button', 'Log in').click();
 
     // check on Status, logged in
