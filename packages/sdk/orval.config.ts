d18ab823c3230bf7184bcd65f46f0d9decf05ad2
Wes <76014409+Redtigercod4@users.noreply.github.com>
2026-09-24 09:49:41 +0000

    Extend base tsconfig, added orval configuration

---
+++ packages/sdk/orval.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'orval';
+
+const targetUrl = 'http://localhost:8080/pulp/api/v3/docs/api.json?pk_path=1';
+
+export default defineConfig({
+  core: {
+    input: {
+      target: targetUrl,
+      filters: {
+        mode: 'include',
+        tags: ['Status'],
+      },
+    },
+    output: {
+      client: 'fetch',
+      mode: 'tags-split',
+      target: './src/core/core.ts',
+      schemas: { path: './src/core/models', splitByTags: true },
+      indexFiles: false,
+    },
+  },
+});
